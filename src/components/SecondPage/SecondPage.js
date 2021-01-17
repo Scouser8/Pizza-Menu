7d3a9eb428ab7d77fac552478b5b794280b362d5
Muhammad Abd Al-Baqi <45911940+Scouser8@users.noreply.github.com>
2021-01-17 07:50:50 +0200

    Menu-SecondPage-Edited

--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -32,52 +32,68 @@ export default function SecondPage() {
           </div>
         </div>
       </div>
-        <div className="secondPage__sectionTwo__body">
-          <img className="secondPage__body__img" src={Pizza} alt="" srcset="" />
-          <div className="secondPage__body__title">
-            <span className="secondPage__body__title__content">
-              <span style={{ fontWeight: "normal" }}>#</span>pizzeria.
+      <div className="secondPage__sectionTwo__body">
+        <img className="secondPage__body__img" src={Pizza} alt="" srcset="" />
+        <div className="secondPage__body__title">
+          <span className="secondPage__body__title__content">
+            <span style={{ fontWeight: "normal" }}>#</span>pizzeria.
+          </span>
+        </div>
+        <div className="secondPage__body__menuTitle">
+          <div className="secondPage__body__menuTitle__content__container">
+            <span className="secondPage__body__menuTitle__content">
+              our new menu
             </span>
           </div>
-          <div className="secondPage__body__menuTitle">
-            <div className="secondPage__body__menuTitle__content__container">
-              <span className="secondPage__body__menuTitle__content">
-                our new menu
-              </span>
-            </div>
-          </div>
-          <div className="secondPage__body__menuBackground"></div>
-          <div className="secondPage__body__menu">
-            <p className="whats__new__content">
+        </div>
+        <div className="secondPage__body__menuBackground"></div>
+        <div className="secondPage__menu__priceBG"></div>
+        <div className="secondPage__body__menu">
+          <div className="whats__new__content">
+            <span>
               <span style={{ fontWeight: 900, marginRight: "20px" }}>|</span>
-              EUISMOD SED DO <br />
-              (tempor, incididunt, utas, labore, etws)
-            </p>
-            <br />
-            <p className="whats__new__content">
-              <span style={{ fontWeight: 900, marginRight: "20px" }}>||</span>
-              Do Dolore <br /> (magna, aliqua, lorem)
-            </p>
-            <br />
-            <p className="whats__new__content">
-              <span style={{ fontWeight: 900, marginRight: "20px" }}>|||</span>
-              EIUSMOD SED DO <br />
-              (tempor, incididunt, utas, labore, etws)
-            </p>
-            <br />
-            <p className="whats__new__content">
-              <span style={{ fontWeight: 900, marginRight: "20px" }}>||</span>
-              Do Dolore <br /> (magna, aliqua, lorem)
-            </p>
+              EUISMOD SED DO
+            </span>
+            <span>10$</span>
           </div>
-          <div className="secondPage__bottom">
-            <div className="secondPage__bottom__content">
-              <p>||| _ top in menu</p>
-              <p>|| _ sale item</p>
-              <p>| _ made by shef</p>
-            </div>
+          (tempor, incididunt, utas, labore, etws)
+          <br /> <br />
+          <div className="whats__new__content">
+            <span>
+              <span style={{ fontWeight: 900, marginRight: "20px" }}>||</span>
+              EUISMOD SED DO
+            </span>
+            <span>14$</span>
+          </div>
+          (magna, aliqua, lorem)
+          <br /> <br />
+          <div className="whats__new__content">
+            <span>
+              <span style={{ fontWeight: 900, marginRight: "20px" }}>|||</span>
+              EUISMOD SED DO
+            </span>
+            <span>20$</span>
+          </div>
+          (tempor, incididunt, utas, labore, etws)
+          <br /> <br />
+          <div className="whats__new__content">
+            <span>
+              <span style={{ fontWeight: 900, marginRight: "20px" }}>||</span>
+              EUISMOD SED DO
+            </span>
+            <span>10$</span>
+          </div>
+          (magna, aliqua, lorem)
+        </div>
+
+        <div className="secondPage__bottom">
+          <div className="secondPage__bottom__content">
+            <p>||| _ top in menu</p>
+            <p>|| _ sale item</p>
+            <p>| _ made by shef</p>
           </div>
         </div>
+      </div>
     </Fragment>
   );
 }
